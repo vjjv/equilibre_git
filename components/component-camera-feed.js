aba1dfdc010acaf3c31d547a40305bf6dcbcbc24
Bastien Sarolea <sarobastien@gmail.com>
2025-03-15 15:12:38 +0100

    01

--- components/component-camera-feed.js
+++ components/component-camera-feed.js
@@ -6,36 +6,36 @@ AFRAME.registerComponent('camera-feed', {
         var scene = this.el.sceneEl;
         var camera = scene.camera;
 
-        // var video = document.createElement('video');
-        // video.setAttribute('autoplay', '');
-        // video.setAttribute('muted', '');
-        // video.setAttribute('playsinline', '');
-        // video.setAttribute('id', 'video-camera-feed');
+        var video = document.createElement('video');
+        video.setAttribute('autoplay', '');
+        video.setAttribute('muted', '');
+        video.setAttribute('playsinline', '');
+        video.setAttribute('id', 'video-camera-feed');
 
-        // video.style.position = 'fixed';
-        // video.style.top = '0';
-        // video.style.left = '0';
-        // video.style.width = '100%';
-        // video.style.height = '100%';
-        // video.style.objectFit = 'cover';
-        // video.style.zIndex = '2';
-        // video.style.opacity = '0.9';
-        // video.style.pointerEvents = 'none';
+        video.style.position = 'fixed';
+        video.style.top = '0';
+        video.style.left = '0';
+        video.style.width = '100%';
+        video.style.height = '100%';
+        video.style.objectFit = 'cover';
+        video.style.zIndex = '2';
+        video.style.opacity = '0.9';
+        video.style.pointerEvents = 'none';
 
-        // // Apply mask using CSS
-        // video.style.webkitMaskImage = 'url(assets/mask.png)';
-        // video.style.maskImage = 'url(assets/mask.png)';
-        // video.style.webkitMaskSize = 'cover';
-        // video.style.maskSize = 'cover';
-        // video.style.webkitMaskRepeat = 'no-repeat';
-        // video.style.maskRepeat = 'no-repeat';
-        // video.style.webkitMaskPosition = 'center';
-        // video.style.maskPosition = 'center';
-        // video.classList.add('hidden');
-        // video.style.display = 'none';
+        // Apply mask using CSS
+        video.style.webkitMaskImage = 'url(assets/mask.png)';
+        video.style.maskImage = 'url(assets/mask.png)';
+        video.style.webkitMaskSize = 'cover';
+        video.style.maskSize = 'cover';
+        video.style.webkitMaskRepeat = 'no-repeat';
+        video.style.maskRepeat = 'no-repeat';
+        video.style.webkitMaskPosition = 'center';
+        video.style.maskPosition = 'center';
+        video.classList.add('hidden');
+        video.style.display = 'none';
 
 
-        // document.body.appendChild(video);
+        document.body.appendChild(video);
         document.getElementById('ortho').appendChild(video);
 
         navigator.mediaDevices.getUserMedia({
